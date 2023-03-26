ea3dce7b7c7898c7194528e046010764c25ce298
Daniel Ulises <daniel.cau@pm.me>
2023-03-26 22:58:37 +0200

    Moved the Healing Herb View to a separate file

---
+++ HealingHerbMissionView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Roster;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.MissionViews;
+using TaleWorlds.ObjectSystem;
+
+namespace WitchesOfCalradia
+{
+    public class HealingHerbMissionView : MissionView
+    {
+        public override void OnMissionScreenTick(float dt)
+        {
+            base.OnMissionScreenTick(dt);
+            
+            if (Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q))
+            {
+                EatHealingHerb();
+            }
+        }
+
+        private void EatHealingHerb()
+        {
+            Agent agent = Agent.Main;
+            ItemRoster items = MobileParty.MainParty.ItemRoster;
+            ItemObject healingHerb = MBObjectManager.Instance.GetObject<ItemObject>("woc_healing_herb");
+
+            if (!(Mission.Mode is MissionMode.Battle or MissionMode.Stealth or MissionMode.Duel)) return;
+
+            if (items.GetItemNumber(healingHerb) == 0)
+            {
+                InformationManager.DisplayMessage(new InformationMessage("You ran out of herbs, go visit a witch to restock"));
+            }
+            else if (agent.Health == agent.BaseHealthLimit)
+            {
+                InformationManager.DisplayMessage(new InformationMessage("The herb wouldn't have any effect... You decide not to eat it."));
+            }
+            else if(agent.BaseHealthLimit <= 10)
+            {
+                InformationManager.DisplayMessage(new InformationMessage("You're too weak, the plant would kill you"));
+            }
+            else if ((agent.BaseHealthLimit - 50) <= 10)
+            {
+                items.AddToCounts(healingHerb, -1);
+                agent.BaseHealthLimit = 10;
+                InformationManager.DisplayMessage(new InformationMessage(
+                    String.Format("You gather your last bit of strenght to eat a healing herb.\nYour maximum health is now {0}", agent.BaseHealthLimit)
+                    ));
+            }
+            else
+            {
+                items.AddToCounts(healingHerb, -1);
+                agent.BaseHealthLimit -= 30;
+                agent.Health = agent.BaseHealthLimit;
+                InformationManager.DisplayMessage(new InformationMessage(
+                    String.Format("You restored your health, but you suffer side effects!\nYour maximum health is now {0}", agent.BaseHealthLimit)
+                    ));
+            }
+
+        }
+    }
+}
--- SubModule.cs
+++ SubModule.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using TaleWorlds.CampaignSystem.Party;
-using TaleWorlds.CampaignSystem.Roster;
+﻿using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
-using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.MountAndBlade.View.MissionViews;
-using TaleWorlds.ObjectSystem;
 
 namespace WitchesOfCalradia
 {
@@ -23,58 +18,15 @@ namespace WitchesOfCalradia
             mission.AddMissionBehavior(new HealingHerbMissionView());
         }
 
-    }
-
-    public class HealingHerbMissionView : MissionView
-    {
-        public override void OnMissionScreenTick(float dt)
+        protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
         {
-            base.OnMissionScreenTick(dt);
-            
-            if (Input.IsKeyPressed(TaleWorlds.InputSystem.InputKey.Q))
+            base.InitializeGameStarter(game, starterObject);
+
+            if (starterObject is CampaignGameStarter starter)
             {
-                EatHealingHerb();
+                starter.AddBehavior(new WitchesOfCalradiaBehavior());
             }
         }
 
-        private void EatHealingHerb()
-        {
-            Agent agent = Agent.Main;
-            ItemRoster items = MobileParty.MainParty.ItemRoster;
-            ItemObject healingHerb = MBObjectManager.Instance.GetObject<ItemObject>("woc_healing_herb");
-
-            if (!(Mission.Mode is MissionMode.Battle or MissionMode.Stealth or MissionMode.Duel)) return;
-
-            if (items.GetItemNumber(healingHerb) == 0)
-            {
-                InformationManager.DisplayMessage(new InformationMessage("You ran out of herbs, go visit a witch to restock"));
-            }
-            else if (agent.Health == agent.BaseHealthLimit)
-            {
-                InformationManager.DisplayMessage(new InformationMessage("The herb wouldn't have any effect... You decide not to eat it."));
-            }
-            else if(agent.BaseHealthLimit <= 10)
-            {
-                InformationManager.DisplayMessage(new InformationMessage("You're too weak, the plant would kill you"));
-            }
-            else if ((agent.BaseHealthLimit - 50) <= 10)
-            {
-                items.AddToCounts(healingHerb, -1);
-                agent.BaseHealthLimit = 10;
-                InformationManager.DisplayMessage(new InformationMessage(
-                    String.Format("You gather your last bit of strenght to eat a healing herb.\nYour maximum health is now {0}", agent.BaseHealthLimit)
-                    ));
-            }
-            else
-            {
-                items.AddToCounts(healingHerb, -1);
-                agent.BaseHealthLimit -= 30;
-                agent.Health = agent.BaseHealthLimit;
-                InformationManager.DisplayMessage(new InformationMessage(
-                    String.Format("You restored your health, but you suffer side effects!\nYour maximum health is now {0}", agent.BaseHealthLimit)
-                    ));
-            }
-
-        }
     }
 }
